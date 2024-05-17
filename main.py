6f46176ca66e5e9d335136ffeb0572d83e5c2fdc
¨stefania¨ <estefaniahurtadog@javerianacali.edu.co>
2024-05-16 21:32:12 -0500

    'arreglos'

--- main.py
+++ main.py
@@ -14,23 +14,38 @@ DB_NAME = 'procesosFinal'
 DB_USER = 'postgres'
 DB_PASS = 'Javeriana1299'
 
+ubi = ["Bodega","Bodega","Bodega","Bodega"]
 conn = psycopg2.connect(dbname=DB_NAME, user = DB_USER, password = DB_PASS, host = DB_HOST)
+int1 = ["Reparacion, 01/06/2024, Ing Stefania: Cambio de aspa por rotura",
+		"Actualización 16/05/2024, Ing Juan Jose: ajustes de seguridad"]
+int2 = ["Reparacion 02/03/2024, Ing Johan: Cambio de aspa por rotura",
+		"Reparacion 18/03/2024, Ing Crhistian: Cambio de camara"]
+int3 = ["Reparacion 02/03/2024, Ing Johan: Cambio de llanta"]
+int4 = ["No hay mantenimientos hasta el momento"]
 # Información
-dron1 = ["50m", "Juan David Aycardi", 
-		 "5 m/s", "documentos académicos de la facultad de ingeniería"]
-dron2 = ["45m", "Jedemías Villarica",
-		 "25 m/s", "comida de la cafetería, papas con queso y tocino"]
-envio1 = ["4565", "Juan David Aycardi",
+dron1 = ["50m", "1", 
+            "15 m/s", "Dron de color plateado,rapido, para carga liviana,camara 4k","Dron",ubi[0]]
+
+dron2 = ["45m", "2",
+            "5 m/s", "Dron de color blanco,velocidad moderada, para carga liviana,camara 2k","Dron",ubi[1]]
+
+robot1 = ["0", "3",
+            "2.7 m/s", "Robot de color negro, velocidad baja, semi-autonomo","Robot",ubi[2]]
+
+robot2 = ["0", "4",
+            "3.5 m/s", "Robot de color blanco y negro, velocidad media, semi-autonomo","Robot",ubi[3]]
+
+envio1 = ["4565", "Kafeliest",
 		  "documentos académicos", "Entrada Principal",
 		  "formatos de despido para el profesor encargado de Sistemas Inteligentes"]
-envio2 = ["6969", "Jedemías Villarica",
+envio2 = ["6969", "Juan Marin",
 		  "comida de la cafetería, papas con queso y tocino", "Portería Cedro",
 		  "Hambre"]
 metros = 100	 
 
 # Variables Globales
 fallo = False
-usuarios = {"messirve": "Luis"}
+usuarios = {"messirve": "Stefania"}
 contrasenas = {"messirve": "1234"}
 usuario_activo = ""
 s3 = boto3.client('s3',
@@ -96,30 +111,43 @@ def home():
     return render_template("home.html", usuario = usuario_activo)
 
 # Drones
+
+# Drones
 @app.route("/drones", methods = ["GET", "POST"])
 def drones():
+
 	return render_template("drones.html", usuario = usuario_activo)
 
 @app.route("/drones/1", methods = ["GET", "POST"])
 def dron_aumentado1():
+	
 	return render_template("dron_aumentado.html", usuario = usuario_activo, numero = "1",
-												  info = dron1)
+												  info = dron1,mant =int1 )
 
 @app.route("/drones/2", methods = ["GET", "POST"])
 def dron_aumentado2():
 	return render_template("dron_aumentado.html", usuario = usuario_activo, numero = "2",
-												  info = dron2)
+												  info = dron2,mant =int2)
+
+@app.route("/drones/3", methods = ["GET", "POST"])
+def dron_aumentado3():
+	return render_template("dron_aumentado.html", usuario = usuario_activo, numero = "3",
+												  info = robot1,mant =int3)
+@app.route("/drones/4", methods = ["GET", "POST"])
+def dron_aumentado4():
+	return render_template("dron_aumentado.html", usuario = usuario_activo, numero = "4",
+												  info = robot2,mant = int4)
 
 # Reservas
 @app.route("/reservas", methods = ["GET", "POST"])
 def reservas():
 	return render_template("reserva.html", usuario = usuario_activo)
 
-'''@app.route("/reservaRegis", methods=["GET","POST"])
+@app.route("/reservaRegis", methods=["GET", "POST"])
 def registrar_reserva():
     if request.method == "POST":
         # Obtener los datos del formulario enviado por el usuario
-        id_reserva = request.form.get("id")
+        id_reserva = request.form.get("id_reserva")
         usuario_id = request.form.get("usuario_id")
         altura = request.form.get("altura")
         velocidad = request.form.get("velocidad")
@@ -135,9 +163,14 @@ def registrar_reserva():
         except Exception as e:
             conn.rollback()
             mensaje = f"Error al registrar la reserva: {str(e)}"
+        finally:
+            cur.close()
 
         # Renderizar la plantilla de respuesta
-        return render_template("reservaRegis.html", mensaje=mensaje)'''
+        return render_template("reserva.html", mensaje=mensaje)
+
+    # Si no es una solicitud POST, solo renderizar la plantilla de formulario
+    return render_template("reserva.html")
 
 
 # Envios
@@ -153,7 +186,7 @@ def envio_aumentado1():
 def envio_aumentado2():
 	return render_template("envio_aumentado.html", usuario = usuario_activo, envio = envio2)
 
-@app.route("/reservaRegis", methods = ["GET", "POST"])
+'''@app.route("/reservaRegis", methods = ["GET", "POST"])
 def reservas1():
     lugares = { '0': "Entrada Central", '1':"Portería Acacias", '2':"Portería Lago", '3':"Portería Cedro" }
     if request.method == 'POST':
@@ -177,7 +210,7 @@ def reservas1():
             except Exception as e:
                 logging.error('Error al cargar el archivo en S3: {}'.format(e))
 
-    return render_template("reserva.html", usuario = usuario_activo)
+    return render_template("reserva.html", usuario = usuario_activo)'''
 
 # Rutas
 @app.route("/rutas", methods = ["GET", "POST"])
